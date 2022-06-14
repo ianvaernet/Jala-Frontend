473903ab827e2573e3b04cd7f354f8509bc9a43f
Ian Vaernet <ianvaernet@gmail.com>
2022-06-14 19:59:19 -0300

    Angular challenge improvements

--- Class35-Angular/project/src/app/todo-list/todo-list.component.ts
+++ Class35-Angular/project/src/app/todo-list/todo-list.component.ts
@@ -4,12 +4,20 @@ import { ToDo } from '../app.component';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  // styleUrls: ['./list-item.component.scss'],
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
   @Input() toDos!: ToDo[];
+  pendingToDos: ToDo[] = [];
+
+  toggleCompleted(index: number) {
+    this.toDos[index].completed = !this.toDos[index].completed;
+    if (this.toDos) {
+      this.pendingToDos = this.toDos.filter((todo) => !todo.completed);
+    }
+  }
 
   deleteToDo(index: number) {
-    console.log('deleteToDo');
+    this.toDos.splice(index, 1);
   }
 }

48db8ea1234636b1d9f6a7c7d9f0760c301fe5fd
Ian Vaernet <ianvaernet@gmail.com>
2022-06-19 19:22:19 -0300

    Add getPokemonImageUri function

--- PokedexAngular/src/app/app.component.ts
+++ PokedexAngular/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'PokedexAngular';
+  getPokemonImageUri(id: number) {
+    const imageId = ('00' + id).slice(-3); // para 1 => 001
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${imageId}.png`;
+  }
 }

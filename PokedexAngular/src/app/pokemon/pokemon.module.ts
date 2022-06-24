6ccea60afacdb190e327b0205cf257b65717ebe5
Ian Vaernet <ianvaernet@gmail.com>
2022-06-24 17:00:29 -0300

    Add search bar component

--- PokedexAngular/src/app/pokemon/pokemon.module.ts
+++ PokedexAngular/src/app/pokemon/pokemon.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [PokemonCardComponent, PokemonListComponent],
+  declarations: [
+    PokemonCardComponent,
+    PokemonListComponent,
+    SearchBarComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   exports: [PokemonListComponent],
   providers: [],

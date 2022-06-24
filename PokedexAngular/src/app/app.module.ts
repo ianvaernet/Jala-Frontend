29615c0a786396bbb1b481c43b2b82b327940acd
Ian Vaernet <ianvaernet@gmail.com>
2022-06-24 16:01:05 -0300

    Add empty footer and header

--- PokedexAngular/src/app/app.module.ts
+++ PokedexAngular/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, PokemonModule],
   providers: [],
   bootstrap: [AppComponent],

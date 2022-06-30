54296f8ede540a1d7a3e5ad9bb3db8a44163406c
Ian Vaernet <ianvaernet@gmail.com>
2022-06-30 18:49:05 -0300

    Go back from pokemon details

--- PokedexAngular/src/app/pokemon/pokemon-details/pokemon-details.component.ts
+++ PokedexAngular/src/app/pokemon/pokemon-details/pokemon-details.component.ts
@@ -1,15 +1,17 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-details',
   templateUrl: './pokemon-details.component.html',
-  styleUrls: ['./pokemon-details.component.scss']
+  styleUrls: ['./pokemon-details.component.scss'],
 })
 export class PokemonDetailsComponent implements OnInit {
+  constructor(private location: Location) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goBack(): void {
+    this.location.back();
   }
-
 }

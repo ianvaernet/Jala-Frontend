737329338e0f1cd4c0a229990140fed3714e0be3
Ian Vaernet <ianvaernet@gmail.com>
2022-07-05 15:35:32 -0300

    Add angular material

--- PokedexAngular/src/app/app.module.ts
+++ PokedexAngular/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, PokemonModule],
+  imports: [BrowserModule, PokemonModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })

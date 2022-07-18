57b49168211474c16aa358aa3bf2df3207db746d
Ian Vaernet <ianvaernet@gmail.com>
2022-07-18 19:18:55 -0300

    Github Pages

--- Class02-Javascript/challenge.js
+++
@@ -1,5 +0,0 @@
-const findUnique = (array) => array.find((element) => array.indexOf(element) === array.lastIndexOf(element));
-const findUnique2 = (array) => array.reduce((i, j) => i ^ j);
-
-console.log(findUnique([1, 2, 4, 7, 4, 2, 1]));
-console.log(findUnique2([1, 2, 4, 7, 4, 2, 1]));
